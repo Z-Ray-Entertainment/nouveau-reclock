9f6eeb3ab9ebdc0489c5e304e272e9e059fe294c
Vortex Acherontic <vortex@z-ray.de>
2024-08-07 12:20:02 +0200

    Better docs

--- nvreclock/utils.py
+++ nvreclock/utils.py
@@ -2,16 +2,21 @@ import os
 import subprocess
 
 from nvreclock.class_gpu import GPU
-from nvreclock.const import NVIDIA_VENDOR_ID, DEVICE_CLASS_GPU, PCI_DEVICE_PATH
+from nvreclock.const import NVIDIA_VENDOR_ID, DEVICE_CLASS_GPU, PCI_DEVICE_PATH, DRIVER_ID
 from nvreclock.pci_ids import get_name, is_supported
 
+found_devices = []
 
-# DRIVER=nouveau
-# PCI_CLASS=30200
-# PCI_ID=10DE:1290
-# PCI_SUBSYS_ID=1462:10B8
-# PCI_SLOT_NAME=0000:01:00.0
-# MODALIAS=pci:v000010DEd00001290sv00001462sd000010B8bc03sc02i00
+"""
+PciDeviceInfo Helper class build using uevent
+A typical uevent looks like:
+DRIVER=nouveau
+PCI_CLASS=30200
+PCI_ID=10DE:1290 VENDOR_ID:DEVICE_ID
+PCI_SUBSYS_ID=1462:10B8
+PCI_SLOT_NAME=0000:01:00.0
+MODALIAS=pci:v000010DEd00001290sv00001462sd000010B8bc03sc02i00
+"""
 class PciDeviceInfo:
     def __init__(self, device_uevent_result):
         device_uevent = device_uevent_result.stdout.decode("utf-8").rstrip()
@@ -31,13 +36,20 @@ class PciDeviceInfo:
                 case "PCI_SLOT_NAME":
                     self.bus_id = property[1]
 
+    """
+    Returns the current PCI device as a GPU device if it is supported.
+    """
     def get_gpu_device(self):
-        if self.vendor == NVIDIA_VENDOR_ID and self.pci_class in DEVICE_CLASS_GPU and is_supported(self.device_id):
+        if self.vendor == NVIDIA_VENDOR_ID and self.pci_class in DEVICE_CLASS_GPU and is_supported(
+                self.device_id) and self.driver == DRIVER_ID:
             return GPU(0, self.device_id, get_name(self.device_id), self.bus_id)
         else:
             return None
 
 
+"""
+Iterates over all pci devices and collects supported GPUs in found_devices
+"""
 def find_gpus():
     for subdir, dirs, files in os.walk(PCI_DEVICE_PATH):
         for device in dirs:
@@ -45,7 +57,12 @@ def find_gpus():
             pci_device_info = PciDeviceInfo(device_uevent_result, )
             gpu_device: GPU = pci_device_info.get_gpu_device()
             if gpu_device is not None:
-                gpu_device.print_gpu()
+                found_devices.append(gpu_device)
 
 
 find_gpus()
+if len(found_devices) == 0:
+    print("No supported devices found")
+else:
+    for gpu in found_devices:
+        gpu.print_gpu()
