332a2214f281cb1060a5b1ee37ca6899624a6f7a
Vortex Acherontic <vortex@z-ray.de>
2024-08-07 18:06:53 +0200

    Clean up after flatpak build

--- flatpak_build/build.sh
+++ flatpak_build/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-#!/usr/bin/bash
 flatpak-builder --force-clean --sandbox --user --install --install-deps-from=flathub --ccache --mirror-screenshots-url=https://dl.flathub.org/media/ --repo=repo builddir ../de.z_ray.Nvreclock.json
+rm -rf ./repo ./builddir ./.flatpak-builder
