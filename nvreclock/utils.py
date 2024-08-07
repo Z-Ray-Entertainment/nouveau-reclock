9965f92ec07ea8c66e29a9d947bd5d53e7b2e8bc
Vortex Acherontic <vortex@z-ray.de>
2024-08-07 12:30:33 +0200

    Update docs

--- nvreclock/utils.py
+++ nvreclock/utils.py
@@ -13,8 +13,8 @@ class PciDeviceInfo:
     PciDeviceInfo Helper class build using uevent
     A typical uevent looks like:
     DRIVER=nouveau -> This is the driver in use for this device
-    PCI_CLASS=30200 -> This tells what kind of pci device this is. Desktop GPU Mobile GPU, WiFi Card, Sound card etc.
-    PCI_ID=10DE:1290 -> VENDOR_ID:DEVICE_ID as ex values
+    PCI_CLASS=30200 -> This tells what kind of pci device this is. Desktop GPU Mobile GPU, Wi-Fi Card, Sound card etc.
+    PCI_ID=10DE:1290 -> VENDOR_ID:DEVICE_ID as hex values
     PCI_SUBSYS_ID=1462:10B8 -> IDK
     PCI_SLOT_NAME=0000:01:00.0 -> PCI bus id. This tells where and how this device is connected to the CPU
     MODALIAS=pci:v000010DEd00001290sv00001462sd000010B8bc03sc02i00 -> IDK
@@ -24,19 +24,19 @@ class PciDeviceInfo:
         device_uevent = device_uevent_result.stdout.decode("utf-8").rstrip()
         device_info = device_uevent.split("\n")
         for properties in device_info:
-            property = properties.split("=")
+            prop = properties.split("=")
 
-            match property[0]:
+            match prop[0]:
                 case "PCI_CLASS":
-                    self.pci_class = property[1]
+                    self.pci_class = prop[1]
                 case "DRIVER":
-                    self.driver = property[1]
+                    self.driver = prop[1]
                 case "PCI_ID":
-                    pci_id = property[1].split(":")
+                    pci_id = prop[1].split(":")
                     self.vendor = pci_id[0]
                     self.device_id = pci_id[1]
                 case "PCI_SLOT_NAME":
-                    self.bus_id = property[1]
+                    self.bus_id = prop[1]
 
     def get_gpu_device(self):
         """
