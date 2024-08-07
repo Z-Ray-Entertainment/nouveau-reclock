5305c6e186d99bc8b77d68abf364a53da13e7bcc
Vortex Acherontic <vortex@z-ray.de>
2024-08-07 12:27:41 +0200

    Update docs

--- nvreclock/utils.py
+++ nvreclock/utils.py
@@ -7,17 +7,19 @@ from nvreclock.pci_ids import get_name, is_supported
 
 found_devices = []
 
-"""
-PciDeviceInfo Helper class build using uevent
-A typical uevent looks like:
-DRIVER=nouveau
-PCI_CLASS=30200
-PCI_ID=10DE:1290 VENDOR_ID:DEVICE_ID
-PCI_SUBSYS_ID=1462:10B8
-PCI_SLOT_NAME=0000:01:00.0
-MODALIAS=pci:v000010DEd00001290sv00001462sd000010B8bc03sc02i00
-"""
+
 class PciDeviceInfo:
+    """
+    PciDeviceInfo Helper class build using uevent
+    A typical uevent looks like:
+    DRIVER=nouveau -> This is the driver in use for this device
+    PCI_CLASS=30200 -> This tells what kind of pci device this is. Desktop GPU Mobile GPU, WiFi Card, Sound card etc.
+    PCI_ID=10DE:1290 -> VENDOR_ID:DEVICE_ID as ex values
+    PCI_SUBSYS_ID=1462:10B8 -> IDK
+    PCI_SLOT_NAME=0000:01:00.0 -> PCI bus id. This tells where and how this device is connected to the CPU
+    MODALIAS=pci:v000010DEd00001290sv00001462sd000010B8bc03sc02i00 -> IDK
+    """
+
     def __init__(self, device_uevent_result):
         device_uevent = device_uevent_result.stdout.decode("utf-8").rstrip()
         device_info = device_uevent.split("\n")
@@ -36,10 +38,10 @@ class PciDeviceInfo:
                 case "PCI_SLOT_NAME":
                     self.bus_id = property[1]
 
-    """
-    Returns the current PCI device as a GPU device if it is supported.
-    """
     def get_gpu_device(self):
+        """
+        Returns the current PCI device as a GPU device if it is supported.
+        """
         if self.vendor == NVIDIA_VENDOR_ID and self.pci_class in DEVICE_CLASS_GPU and is_supported(
                 self.device_id) and self.driver == DRIVER_ID:
             return GPU(0, self.device_id, get_name(self.device_id), self.bus_id)
@@ -47,10 +49,10 @@ class PciDeviceInfo:
             return None
 
 
-"""
-Iterates over all pci devices and collects supported GPUs in found_devices
-"""
 def find_gpus():
+    """
+    Iterates over all pci devices and collects supported GPUs in found_devices
+    """
     for subdir, dirs, files in os.walk(PCI_DEVICE_PATH):
         for device in dirs:
             device_uevent_result = subprocess.run(["cat", PCI_DEVICE_PATH + device + "/uevent"], stdout=subprocess.PIPE)
